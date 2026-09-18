f57deaba72d5836e0d9cb74e96494acc0229430d
fortunekenfack1-beep <fortunekenfack1@gmail.com>
2026-09-18 09:10:18 +0100

    Refactor main.cpp to remove unnecessary whitespace

--- chapitre-01/double-doubler_un_entier/main.cpp
+++ chapitre-01/double-doubler_un_entier/main.cpp
@@ -1,17 +1,9 @@
 #include <stdio.h >
 
 int main () {
-
     int n ;
-
-    printf ("entrer un  nombre entier  \n ") ;
-    
+    printf ("entrer un  nombre entier  \n ") ; 
     scanf("%d" , & n) ;
-
     printf("le double de ce nombre est : %d " ,   n*2 );
-
-    
     return 0 ;
-    
-
 }
