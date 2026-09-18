b44b71844ed5222b36f0fffcae0aa9cd84d78b47
fortunekenfack1-beep <fortunekenfack1@gmail.com>
2026-09-18 09:57:15 +0100

    Fix prompt message for integer input

--- chapitre-01/double-doubler_un_entier/main.cpp
+++ chapitre-01/double-doubler_un_entier/main.cpp
@@ -2,7 +2,7 @@
 
 int main () {
     int n ;
-    printf ("entrer un  nombre entier  \n ") ; 
+    printf ("entrer un  nombre entier  ") ; 
     scanf("%d" , & n) ;
     printf("le double de ce nombre est : %d " ,   n*2 );
     return 0 ;
